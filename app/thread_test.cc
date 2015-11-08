274bd377eb5a9d72cf5908d8776900b5d78d1681
Quenio dos Santos <quenio@me.com>
2015-11-08 11:39:38 -0200

    Quatro threads no thread_test.

--- app/thread_test.cc
+++ app/thread_test.cc
@@ -10,9 +10,13 @@ const int iterations = 10;
 
 int func_a(void);
 int func_b(void);
+int func_c(void);
+int func_d(void);
 
 Thread * a;
 Thread * b;
+Thread * c;
+Thread * d;
 Thread * m;
 
 OStream cout;
@@ -28,15 +32,23 @@ int main()
 
     a = new Thread(&func_a);
     b = new Thread(&func_b);
+    c = new Thread(&func_c);
+    d = new Thread(&func_d);
 
     int status_a = a->join();
     int status_b = b->join();
+    int status_c = c->join();
+    int status_d = d->join();
 
-    cout << "Thread A exited with status " << status_a 
-         << " and thread B exited with status " << status_b << "" << endl;
+    cout << "Thread A exited with status " << status_a
+         << " and thread B exited with status " << status_b
+         << " and thread C exited with status " << status_c
+         << " and thread D exited with status " << status_d << "" << endl;
 
     delete a;
     delete b;
+    delete c;
+    delete d;
 
     cout << "It should not be shown on the display!" << endl;
 
@@ -46,23 +58,55 @@ int main()
 int func_a(void)
 {
     for(int i = iterations; i > 0; i--) {
-        for(int i = 0; i < 79; i++)
-            cout << "a";
+        for(int i = 0; i < 19; i++)
+            cout << "a" << Machine::cpu_id();
         cout << "" << endl;
-        Alarm::delay(500000);
+        for(int i = 0; i < 50000; i++) {
+          Alarm::delay(10);
+        }
     }
 
-    return 'A';   
+    return 'A';
 }
 
 int func_b(void)
 {
     for(int i = iterations; i > 0; i--) {
-        for(int i = 0; i < 79; i++)
-            cout << "b";
+        for(int i = 0; i < 19; i++)
+            cout << "b" << Machine::cpu_id();
         cout << "" << endl;
-        Alarm::delay(500000);
+        for(int i = 0; i < 50000; i++) {
+          Alarm::delay(10);
+        }
     }
 
-    return 'B';   
+    return 'B';
+}
+
+int func_c(void)
+{
+    for(int i = iterations; i > 0; i--) {
+        for(int i = 0; i < 19; i++)
+            cout << "c" << Machine::cpu_id();
+        cout << "" << endl;
+        for(int i = 0; i < 50000; i++) {
+          Alarm::delay(10);
+        }
+    }
+
+    return 'C';
+}
+
+int func_d(void)
+{
+    for(int i = iterations; i > 0; i--) {
+        for(int i = 0; i < 19; i++)
+            cout << "d" << Machine::cpu_id();
+        cout << "" << endl;
+        for(int i = 0; i < 50000; i++) {
+          Alarm::delay(10);
+        }
+    }
+
+    return 'D';
 }
