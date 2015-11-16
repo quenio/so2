e8a4ace2ac1317aaf2311433fd4deda36b37f7ce
Quenio dos Santos <quenio@me.com>
2015-11-15 22:15:29 -0200

    Quantum = 100ms

--- app/phil_dinner_traits.h
+++ app/phil_dinner_traits.h
@@ -122,7 +122,7 @@ template<> struct Traits<Thread>: public Traits<void>
     static const bool smp = Traits<System>::multicore;
 
     typedef Scheduling_Criteria::CPU_Bound_RR Criterion;
-    static const unsigned int QUANTUM = 10000; // us
+    static const unsigned int QUANTUM = 100000; // us
 
     static const bool trace_idle = false;
 };
